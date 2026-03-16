ed0ea81715438528a0520919f706220c6785db8b
leonischu <nischal.201423@ncit.edu.np>
2026-03-16 12:40:18 +0545

    AuthService Created

--- GithubClone.Application/Services/AuthServices.cs
+++ GithubClone.Application/Services/AuthServices.cs
@@ -1,8 +1,16 @@
-﻿using GithubClone.Application.DTOs;
+﻿using AutoMapper;
+using GithubClone.Application.DTOs;
+using GithubClone.Application.Interfaces.Repository;
 using GithubClone.Application.Interfaces.Services;
+using GithubClone.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +18,70 @@ namespace GithubClone.Application.Services
 {
     public class AuthServices : IAuthService
     {
-        public Task<string> LoginAsync(LoginDto dto)
+        private readonly IUserRepository _repo;
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+
+
+        public AuthServices(IUserRepository repo, IConfiguration config, IMapper mapper)
         {
-            
+            _repo = repo;
+            _config = config;
+            _mapper = mapper;
+        }
+        public async Task<UserDto> RegisterAsync(RegisterDto dto)
+        {
+            //Check if user already exists
+            var existingUser = await _repo.GetByEmailAsync(dto.Email);
+            if (existingUser != null)
+                throw new Exception("User already exists");
+
+            //Username = dto.Username,
+            //Email = dto.Email,
+
+            // Use IMapper instead of these two 
+
+            var user = _mapper.Map<User>(dto);
+
+
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+                user.CreatedAt = DateTime.UtcNow;
+                await _repo.CreateAsync(user);
+
+            return   _mapper.Map<UserDto>(user);
+
+
+
+        }
+        public async Task<string> LoginAsync(LoginDto dto)
+        {
+            var user = await _repo.GetByEmailAsync(dto.Email);
+            if(user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.))
+            {
+                throw new Exception("Invalid credintials");
+            }
+
+            var claims = new[]
+            {
+                new Claim("id",user.Id.ToString()),
+                new Claim("username",user.Username),
+                new Claim("email",user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var token = new JwtSecurityToken(
+                     issuer: _config["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials:
+                    new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+
         }
 
-        public Task<UserDto> RegisterAsync(RegisterDto dto)
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
