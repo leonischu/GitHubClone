517d4fc7f7dfc4908a1dbb734004f820ced7f33a
leonischu <nischal.201423@ncit.edu.np>
2026-03-17 17:31:39 +0545

    Service for commit completed

---
+++ GithubClone.Application/Interfaces/Services/ICommitService.cs
@@ -0,0 +1,17 @@
+﻿using GithubClone.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubClone.Application.Interfaces.Services
+{
+    public interface ICommitService
+    {
+        Task CreateCommitAsync(int userId, CreateCommitDto dto);
+
+        Task<IEnumerable<CommitDto>> GetCommits(int repoId);
+    
+}
+}
---
+++ GithubClone.Application/Services/CommitService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using GithubClone.Application.DTOs;
+using GithubClone.Application.Interfaces.Repository;
+using GithubClone.Application.Interfaces.Services;
+using GithubClone.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubClone.Application.Services
+{
+    public class CommitService : ICommitService
+    {
+        private readonly ICommitRepository _commitRepo;
+        private readonly IFileRepository _fileRepo;
+        private readonly IRepositoryFileRepository _repoFileRepo;
+        private readonly IMapper _mapper;
+
+        public CommitService(
+        ICommitRepository commitRepo,
+        IFileRepository fileRepo,
+        IRepositoryFileRepository repoFileRepo,
+        IMapper mapper)
+        {
+            _commitRepo = commitRepo;
+            _fileRepo = fileRepo;
+             _repoFileRepo = repoFileRepo;
+            _mapper = mapper;
+        }
+
+
+        public async Task CreateCommitAsync(int userId, CreateCommitDto dto)
+        {
+            var commit = new Commit
+            {
+                RepositoryId = dto.RepositoryId,
+                Message = dto.Message,
+                CreatedBy = userId,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            var commitId = await _commitRepo.CreateAsync(commit);
+            foreach (var fileDto in dto.Files)
+            {
+                var file = new FileEntity
+                {
+                    FileName = fileDto.FileName,
+                    Content = fileDto.Content
+                };
+                var fileId = await _fileRepo.CreateAsync(file);
+                var repoFile = new RepositoryFile
+                {
+                    RepositoryId = dto.RepositoryId,
+                    FileId = fileId,
+                    CommitId = commitId
+                };
+                await _repoFileRepo.CreateAsync(repoFile);
+
+            }
+        }
+
+        public async Task<IEnumerable<CommitDto>> GetCommits(int repoId)
+        {
+            var commits = await _commitRepo.GetByRepositoryIdAsync(repoId);
+
+            return _mapper.Map<IEnumerable<CommitDto>>(commits);
+        }
+    }
+}
