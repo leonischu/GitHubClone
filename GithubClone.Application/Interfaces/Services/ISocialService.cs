217859cba5405015b0638f8e5f2ce3ff8e58d116
leonischu <nischal.201423@ncit.edu.np>
2026-03-23 11:17:06 +0545

    Service completed for SocialServices

---
+++ GithubClone.Application/Interfaces/Services/ISocialService.cs
@@ -0,0 +1,22 @@
+﻿using GithubClone.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubClone.Application.Interfaces.Services
+{
+    public interface ISocialService
+    {
+        Task StarRepo(int userId, int repoId);
+
+        Task UnstarRepo(int userId, int repoId);
+        Task<int> GetStars(int repoId);
+
+        Task Follow(int userId, int followingId);
+        Task UnFollow (int userId, int followingId);
+
+        Task<IEnumerable<Repositories>> GetFeed(int userId);
+    }
+}
---
+++ GithubClone.Application/Services/SocialService.cs
@@ -0,0 +1,56 @@
+﻿using GithubClone.Application.Interfaces.Repository;
+using GithubClone.Application.Interfaces.Services;
+using GithubClone.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubClone.Application.Services
+{
+    public class SocialService : ISocialService
+    {
+        private readonly ISocialRepository _repo;
+        public SocialService(ISocialRepository repo)
+        {
+            _repo = repo;
+            
+        }
+        public async Task StarRepo(int userId, int repoId)
+        {
+            await _repo.AddStar(userId, repoId);
+        }
+
+
+        public async Task UnstarRepo(int userId, int repoId)
+        {
+            await  _repo.RemoveStar(userId,repoId);
+        }
+
+        public async Task<int> GetStars(int repoId)
+        {
+            return await _repo.GetStarCount(repoId);
+        }
+
+        public async Task Follow(int userId, int followingId)
+        {
+           await _repo.FollowUser(userId, followingId);
+        }
+
+
+
+        public async Task UnFollow(int userId, int followingId)
+        {
+            await _repo.UnfollowUser(userId, followingId);
+        }
+
+        public async Task<IEnumerable<Repositories>> GetFeed(int userId)
+        {
+            var followingIds = await _repo.GetFollowingIds(userId);
+            return await _repo.GetRepositoriesByUserIds(followingIds);
+        }
+
+
+    }
+}
